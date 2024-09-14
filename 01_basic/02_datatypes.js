a105d5c26022a916d4f9d06f5a4ab6450f738605
datta kakde <85458867+datta184@users.noreply.github.com>
2024-09-14 11:26:44 +0000

    data types

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,15 @@
+"use strict"; // treat all js code as newer version
+//alert (3+3)// there will no alert notification bcz we are using nodejs not browser
+/* code should be readable */
+console.log(3+3)
+
+let name = "black"; // string data type its okay to single quote or double quote
+let age = 19; // integer data type
+let isloggedin = true; // boolean 
+// null ----> its stansalone value ----> representation of empty type 
+let password; // undefined 
+// symbol ----> to find the uniqueness
+/* these are primative data type*/
+
+console.log(typeof(age)); // ---->number
+console.log(typeof null); // ----> its type of object and result is null
