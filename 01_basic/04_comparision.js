6980454f2892c104828255c30b0bc00b2d82d363
datta kakde <85458867+datta184@users.noreply.github.com>
2025-01-20 17:32:24 +0000

    Comparision

---
+++ 01_basic/04_comparision.js
@@ -0,0 +1,14 @@
+console.log(2>1);
+console.log(2<1);
+console.log(2>=1);
+console.log(2<=1);
+
+console.log("----------------");
+
+console.log("2" > 1);
+console.log("2" < 1);
+
+console.log("----------------");
+
+console.log(null > 1);
+console.log(null < 1);
